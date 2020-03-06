cf8bffc3a27f8e3605beec24032b0d2f9fd0a5fd
Fraser Waters <45234716+FraserWaters-GR@users.noreply.github.com>
2020-03-06 16:31:28 +0000

    Improve type of connectMenu
    Changed connectMenu type to specify that the function deals with React components, not just any.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,62 +1,90 @@
 declare module "react-contextmenu" {
-    import * as React from "react";
+  import * as React from "react";
 
-    export interface ContextMenuProps {
-        id: string,
-        data?: any,
-        className?: string,
-        hideOnLeave?: boolean,
-        rtl?: boolean,
-        onHide?: {(event: any): void},
-        onMouseLeave?: {(event: React.MouseEvent<HTMLElement>, data: Object, target: HTMLElement): void} | Function,
-        onShow?: {(event: any): void},
-        preventHideOnContextMenu?: boolean,
-        preventHideOnResize?: boolean,
-        preventHideOnScroll?: boolean,
-        style?: React.CSSProperties,
-    }
+  interface ContextMenuProps {
+    id: string;
+    data?: any;
+    className?: string;
+    hideOnLeave?: boolean;
+    rtl?: boolean;
+    onHide?: { (event: any): void };
+    onMouseLeave?:
+      | {
+          (
+            event: React.MouseEvent<HTMLElement>,
+            data: Object,
+            target: HTMLElement
+          ): void;
+        }
+      | Function;
+    onShow?: { (event: any): void };
+  }
 
-    export interface ContextMenuTriggerProps {
-        id: string,
-        attributes?: React.HTMLAttributes<any>,
-        collect?: {(data: any): any},
-        disable?: boolean,
-        holdToDisplay?: number,
-        renderTag?: React.ReactType,
-        mouseButton?: number,
-        disableIfShiftIsPressed?: boolean,
-    }
+  interface ContextMenuTriggerProps {
+    id: string;
+    attributes?: React.HTMLAttributes<any>;
+    collect?: { (data: any): any };
+    disable?: boolean;
+    holdToDisplay?: number;
+    renderTag?: React.ElementType;
+  }
 
-    export interface MenuItemProps {
-        attributes?: React.HTMLAttributes<HTMLDivElement>,
-        className?: string;
-        data?: Object,
-        disabled?: boolean,
-        divider?: boolean,
-        preventClose?: boolean,
-        onClick?: {(event: React.TouchEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement>, data: Object, target: HTMLElement): void} | Function,
-    }
+  interface MenuItemProps {
+    attributes?: React.HTMLAttributes<HTMLDivElement>;
+    data?: Object;
+    disabled?: boolean;
+    divider?: boolean;
+    preventClose?: boolean;
+    onClick?:
+      | {
+          (
+            event:
+              | React.TouchEvent<HTMLDivElement>
+              | React.MouseEvent<HTMLDivElement>,
+            data: Object,
+            target: HTMLElement
+          ): void;
+        }
+      | Function;
+  }
 
-    export interface SubMenuProps {
-        title: React.ReactElement<any> | React.ReactText,
-        className?: string,
-        disabled?: boolean,
-        hoverDelay?: number,
-        rtl?: boolean,
-        preventCloseOnClick?: boolean,
-        onClick?: {(event: React.TouchEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement>, data: Object, target: HTMLElement): void} | Function,
-    }
+  interface SubMenuProps {
+    title: React.ReactElement<any>;
+    className?: string;
+    disabled?: boolean;
+    hoverDelay?: number;
+    rtl?: boolean;
+  }
 
+  interface ConnectMenuProps {
+    id: string;
+    trigger: any;
+  }
+
+  module ReactContextmenu {
     export const ContextMenu: React.ComponentClass<ContextMenuProps>;
-    export const ContextMenuTrigger: React.ComponentClass<ContextMenuTriggerProps>;
+    export const ContextMenuTrigger: React.ComponentClass<
+      ContextMenuTriggerProps
+    >;
     export const MenuItem: React.ComponentClass<MenuItemProps>;
     export const SubMenu: React.ComponentClass<SubMenuProps>;
-    export function connectMenu(menuId: string): (menu: any) => any;
+    export function connectMenu<P>(
+      menuId: string
+    ): (
+      Child: React.ComponentType<P & ConnectMenuProps>
+    ) => React.ComponentType<P>;
     export function showMenu(opts?: any, target?: HTMLElement): void;
     export function hideMenu(opts?: any, target?: HTMLElement): void;
+  }
+
+  export = ReactContextmenu;
 }
 
 declare module "react-contextmenu/modules/actions" {
+  module ReactContextmenuActions {
     export function showMenu(opts?: any, target?: HTMLElement): void;
     export function hideMenu(opts?: any, target?: HTMLElement): void;
+  }
+
+  export = ReactContextmenuActions;
 }
